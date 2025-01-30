71f02000bf0a7a90dcc68013e4cc20c34b16f580
Emilie Thon <emilie.m.thon@gmail.com>
2025-01-30 11:11:15 +0100

    level 2 no.2: js disable of subtract button added

--- Opgaver/Niveau2/02/script.js
+++ Opgaver/Niveau2/02/script.js
@@ -12,10 +12,13 @@ function addToCounter(e) {
   const amounts = e.target
     .closest(".products__product")
     .querySelector(".products__amount");
-  let count = parseInt(amounts.innerText) || 0;
-  // laver string til et heltal
+  let count = parseInt(amounts.innerText) || 0; // laver string til et heltal, så vi kan tilføje og trække fra
+
   count++;
   amounts.innerHTML = count;
+
+  enableDisable(e);
+  activeClass(e);
 }
 
 function subtractFromCounter(e) {
@@ -26,5 +29,27 @@ function subtractFromCounter(e) {
   if (count > 0) {
     count--;
     amounts.innerHTML = count;
+  }
+
+  enableDisable(e);
+  activeClass(e);
+}
+
+function enableDisable(e) {
+  const subtractBtn = e.target
+    .closest(".products__product")
+    .querySelector(".products__subtract");
+  const amounts = e.target
+    .closest(".products__product")
+    .querySelector(".products__amount");
+  const count = parseInt(amounts.innerText) || 0;
+
+
+  if (count > 0) {
+    subtractBtn.removeAttribute("disabled");
+    subtractBtn.style.color = "black"; /
+  } else {
+    subtractBtn.setAttribute("disabled", true); 
+    subtractBtn.style.color = "transparent";
   }
 }
