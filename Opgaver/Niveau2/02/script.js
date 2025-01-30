ca1a56ef64d67393e67f2bd0ede2f23cb6a7c8f6
Emilie Thon <emilie.m.thon@gmail.com>
2025-01-30 13:49:44 +0100

    level 2 no.2: js for total price added

--- Opgaver/Niveau2/02/script.js
+++ Opgaver/Niveau2/02/script.js
@@ -8,50 +8,51 @@ subtractBtn.forEach(function (btn) {
   btn.addEventListener("click", subtractFromCounter);
 });
 
+//! Tilføje antal ved klik på "+" knap:
 function addToCounter(e) {
-  const amounts = e.target
-    .closest(".products__product")
-    .querySelector(".products__amount");
+  const product = e.target.closest(".products__product");
+  const amounts = product.querySelector(".products__amount");
   let count = parseInt(amounts.innerText) || 0; // laver string til et heltal, så vi kan tilføje og trække fra
 
   count++;
   amounts.innerHTML = count;
 
   enableDisable(e);
+  showPrice(product, count);
 }
 
+//! Fjerne antal ved klik på "-" knap:
 function subtractFromCounter(e) {
-  const amounts = e.target
-    .closest(".products__product")
-    .querySelector(".products__amount");
+  const product = e.target.closest(".products__product");
+  const amounts = product.querySelector(".products__amount");
   let count = parseInt(amounts.innerText) || 0;
+
   if (count > 0) {
     count--;
     amounts.innerHTML = count;
   }
 
   enableDisable(e);
+  showPrice(product, count);
 }
 
+//! enable og disable "-" knappen når 0 rammes:
 function enableDisable(e) {
-  const subtractBtn = e.target
-    .closest(".products__product")
-    .querySelector(".products__subtract");
-  const amounts = e.target
-    .closest(".products__product")
-    .querySelector(".products__amount");
+  const product = e.target.closest(".products__product");
+  const subtractBtn = product.querySelector(".products__subtract");
+  const amounts = product.querySelector(".products__amount");
   const count = parseInt(amounts.innerText) || 0;
-
 
   if (count > 0) {
     subtractBtn.removeAttribute("disabled");
-    subtractBtn.style.color = "black"; 
+    subtractBtn.style.color = "black";
   } else {
-    subtractBtn.setAttribute("disabled", true); 
+    subtractBtn.setAttribute("disabled", true);
     subtractBtn.style.color = "transparent";
   }
 }
 
+//! Tilføje og fjerne BEM modifier (class):
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(function (btn) {
@@ -59,9 +60,21 @@ buttons.forEach(function (btn) {
 });
 
 function activeClass(e) {
-    e.target.classList.add("products__button--active");
+  e.target.classList.add("products__button--active");
 
-    setTimeout(function() {
-        e.target.classList.remove("products__button--active");
-      }, 300); // setTimeout tæller i millisekunder (her er det 300 millisekunder = 0.3 sek)
+  setTimeout(function () {
+    e.target.classList.remove("products__button--active");
+  }, 300); // setTimeout tæller i millisekunder (her er det 300 millisekunder = 0.3 sek)
+}
+
+//! Totale pris:
+function showPrice(productPrice, count) {
+  const priceElement = productPrice.querySelector(".products__price");
+
+  let price = parseInt(priceElement.innerText.replace(/[,-]/g, "").trim()); //innerText.replace skifter ",-" ud med "" hvorefter den trimmer det tomme mellemrum væk, så det kan laves til et tal.
+
+  let total = price * count;
+
+  productPrice.querySelector(".products__total").innerHTML =
+    total + " kr i alt";
 }
