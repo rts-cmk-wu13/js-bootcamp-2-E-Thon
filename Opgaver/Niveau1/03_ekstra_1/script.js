00199d0c4355d99cbdcc061d5712be33ae000883
Emilie Thon <emilie.m.thon@gmail.com>
2025-01-30 14:08:18 +0100

    Level 1, 03-extra: js begun

--- Opgaver/Niveau1/03_ekstra_1/script.js
+++ Opgaver/Niveau1/03_ekstra_1/script.js
@@ -1,6 +1,15 @@
+//! Hide via css selector:
 function skjulElementViaCssSelector(cssSelector) {
   if (document.querySelector(cssSelector))
     document.querySelector(cssSelector).style.display = "none";
 }
 
 skjulElementViaCssSelector(".placeholder__img");
+
+//! Hide via element:
+function skjulElement(element) {
+  if (document.querySelector(element) != null)
+    document.querySelector(element).style.display = "none";
+}
+
+skjulElement(".placeholder__img");
