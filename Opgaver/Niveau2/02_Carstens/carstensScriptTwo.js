372f40c59c935257ac16e04766a03b6ea2f52bfe
Emilie Thon <emilie.m.thon@gmail.com>
2025-01-31 14:06:23 +0100

    level 2, 02: Carstens versions; JS for 3 cards created

--- Opgaver/Niveau2/02_Carstens/carstensScriptTwo.js
+++ Opgaver/Niveau2/02_Carstens/carstensScriptTwo.js
@@ -1,24 +1,27 @@
 //! 3 card:
-let increaseBtn = document.querySelector(".card__increase");
-let decreaseBtn = document.querySelector(".card__decrease");
-let countElm = document.querySelector(".card__item-count");
-let count = Number(countElm.textContent);
-let price = Number(document.querySelector(".card__price-value").textContent);
-let totalPriceElm = document.querySelector(".card__total-price");
+let cards = document.querySelectorAll(".card");
 
-console.log(count);
+cards.forEach(function (card) {
+  // vi vælger elementerne, som ligger inde i .card:
+  let increaseBtn = card.querySelector(".card__increase");
+  let decreaseBtn = card.querySelector(".card__decrease");
+  let countElm = card.querySelector(".card__item-count");
+  let count = Number(countElm.textContent);
+  let price = Number(card.querySelector(".card__price-value").textContent);
+  let totalPriceElm = card.querySelector(".card__total-price");
 
-function updateCountAndPrice(count, price) {
-  countElm.textContent = count;
-  totalPriceElm.textContent = count*price;
-}
+  function updateCountAndPrice(count, price) {
+    countElm.textContent = count;
+    totalPriceElm.textContent = count * price;
+  }
 
-increaseBtn.addEventListener("click", function () {
-  count++;
-  updateCountAndPrice(count, price);
-});
+  increaseBtn.addEventListener("click", function () {
+    count++;
+    updateCountAndPrice(count, price);
+  });
 
-decreaseBtn.addEventListener("click", function () {
-  if (count > 0) count--;
-  updateCountAndPrice(count, price);
+  decreaseBtn.addEventListener("click", function () {
+    if (count > 0) count--;
+    updateCountAndPrice(count, price);
+  });
 });
